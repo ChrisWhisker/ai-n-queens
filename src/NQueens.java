61316fe2641702a969b87dc5de309b4adc81cf4a
ChrisWhisker <chrisworc@gmail.com>
2024-03-05 14:21:31 -0500

    Add project files

---
+++ src/NQueens.java
@@ -0,0 +1,20 @@
+
+public class NQueens {
+
+	public static void main(String[] args) {
+
+		String inputFileName;
+		String algorithm;
+
+		if (args.length == 2) {
+			inputFileName = args[0];
+			algorithm = args[1].toUpperCase();
+		} else {
+			// For testing:
+//			inputFileName = "src/Mazes/BFS/Maze1.txt";
+//			algorithm = "DFS";
+//			System.err.println("Two arguments are required: <fileName.txt> <BFS/DFS/astar>");
+//			return;
+		}
+	}
+}
